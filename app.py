ebb3a588e8e9b636403cac18dcb6b0100e3063a9
sarransulaim <82736275+sarransulaim@users.noreply.github.com>
2025-04-29 02:50:56 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,15 +8,15 @@ def main():
     choice = st.sidebar.selectbox("Select Option", menu)
 
     if choice == "Add Item":
-        1_Add_Item.run()
+        Add_Item.run()
     elif choice == "In/Out Operations":
-        2_In_Out.run()
+        In_Out.run()
     elif choice == "View Inventory":
-        3_View_Inventory.run()
+        View_Inventory.run()
     elif choice == "Retrieve Item":
-        4_Retrieve_Item.run()
+        Retrieve_Item.run()
     elif choice == "Trends":
-        5_Trends.run()
+        Trends.run()
 
 if __name__ == "__main__":
     main()
